0713d8b77661d158400f9622c1c28b375d77669f
Alex Belanger <nitrix@nitrix.me>
2024-10-12 23:15:21 -0400

    Vendoring includes seperatly because of Go not tolerating Git submodules.

---
+++ generator/generator.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+)
+
+func main() {
+	copyFile("thirdparty/cimguizmo/cimguizmo.h", "dist/include/cimguizmo.h")
+}
+
+func copyFile(src, dst string) error {
+	_ = os.MkdirAll(filepath.Dir(dst), 0750)
+	srcFile, err := os.Open(src)
+	if err != nil {
+		return err
+	}
+	defer srcFile.Close()
+
+	dstFile, err := os.Create(dst)
+	if err != nil {
+		return err
+	}
+	defer dstFile.Close()
+
+	_, err = srcFile.WriteTo(dstFile)
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("Copied file %s to %s\n", src, dst)
+
+	return nil
+}
